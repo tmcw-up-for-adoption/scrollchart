16a16b11c6d376e08c1debacd397df3073a3611c
Tom MacWright <tom@macwright.org>
2013-05-21 21:32:11 -0500

    Better demo

--- scrollchart.js
+++ scrollchart.js
@@ -7,7 +7,7 @@ function scrollPercent() {
         (document.documentElement.scrollHeight - window.innerHeight);
 }
 
-function barChart(elem, options) {
+function bar(elem, options) {
     window.addEventListener('load', function() {
         var ctx = elem.getContext('2d');
         var w = ctx.canvas.offsetWidth,
@@ -29,6 +29,48 @@ function barChart(elem, options) {
     });
 }
 
+function pie(elem, options) {
+    window.addEventListener('load', function() {
+        var ctx = elem.getContext('2d');
+        var w = ctx.canvas.offsetWidth,
+            h = ctx.canvas.offsetHeight;
+        ctx.canvas.width = ctx.canvas.offsetWidth;
+        ctx.canvas.height = ctx.canvas.offsetHeight;
+        options = options || {};
+        options.bg = options.bg || '#fff';
+        options.face = options.face || '#eee';
+        options.fg = options.fg || '#fae';
+        window.addEventListener('scroll', function() {
+            var p = scrollPercent();
+            reqFrame(function() {
+                ctx.fillStyle = options.bg;
+                ctx.fillRect(0, 0, w, h);
+
+                ctx.fillStyle = options.face;
+                ctx.beginPath();
+                ctx.arc(
+                    w / 2,
+                    h / 2,
+                    w / 2,
+                    0, Math.PI * 2, false);
+                ctx.fill();
+
+                ctx.fillStyle = options.fg;
+                ctx.beginPath();
+                ctx.moveTo(w / 2, h / 2);
+                ctx.arc(w / 2, h / 2,
+                    w / 2,
+                    Math.PI * (- 0.5 + 2 * 0),
+                    Math.PI * (- 0.5 + 2 * p), false);
+                ctx.lineTo(w / 2, h / 2);
+                ctx.closePath();
+                ctx.fill();
+            });
+        });
+    });
+}
+
 var scrollChart = {
-    barChart: barChart
+    bar: bar,
+    pie: pie
 };
